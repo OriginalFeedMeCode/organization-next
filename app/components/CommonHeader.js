4fc012d9572abf51175246e95451e5c99cfdedd9
Shoaib Ansari <app@internationalschooling.org>
2024-06-02 13:31:45 +0530

    signup content done

--- app/components/CommonHeader.js
+++ app/components/CommonHeader.js
@@ -6,7 +6,7 @@ const CommonHeader = ({ isRegister }) => {
     return (
         <div className='py-4 w-full border-b sticky top-0 left-0 right-0 bg-white/20 backdrop-blur'>
             <div className='r-w flex items-center min-h-max justify-between'>
-                <Link href={LINK_PATHS.HOME}><h2 className='font-bold text-xl md:text-2xl'>Doctors Digital.</h2></Link>
+                <Link href={LINK_PATHS.HOME}><h2 className='font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-800 to-red-800 g-t text-xl md:text-2xl'>Doctors Digital.</h2></Link>
                 {
                     isRegister ? <Link href={LINK_PATHS.HOME} className='btn' >Login</Link> : <Link href={LINK_PATHS.SIGNUP} className='btn' >Register Now</Link>
                 }
--- app/register-now/page.js
+++ app/register-now/page.js
@@ -1,3 +1,4 @@
+import Accordian from '@components/Accordian';
 import CommonFooter from '@components/CommonFooter'
 import CommonHeader from '@components/CommonHeader'
 import React from 'react'
@@ -8,10 +9,40 @@ export const metadata = {
 };
 
 function RegisterNow() {
+
+    const features = [
+        {
+            title: "Effortless Appointment Management",
+            description: "Say goodbye to manual scheduling! Our intuitive appointment management system allows you to schedule, reschedule, and cancel appointments with ease. Spend less time on administrative tasks and more time focusing on patient care."
+        },
+        {
+            title: "Comprehensive Patient Details",
+            description: "Access all your patient information in one centralized location. From medical history to treatment plans, our platform provides comprehensive patient profiles to support informed decision-making and personalized care."
+        },
+        {
+            title: "Seamless Communication",
+            description: "Enhance communication with your patients through secure messaging and telehealth features. Keep your patients informed and engaged every step of the way, whether they're in the clinic or at home."
+        }
+    ];
+
+
+
     return (
         <div className='w-full md:min-h-max h-[100svh] flex flex-col'>
             <CommonHeader isRegister={true} />
-            <div className='flex-1 py-6'></div>
+            <div className='py-8 flex-1 w-full '>
+                <div className='r-w grid grid-cols-1 md:grid-cols-3 gap-5'>
+                    <div className='md:col-span-2'>
+                        <h1 className=' text-xl md:text-2xl font-semibold mb-4 '>Create a New Account with Doctor's Digital</h1>
+                        <p className='mb-0 md:mb-6'>Welcome to Doctor's Digital – where modern healthcare meets seamless management. Join our growing community of healthcare professionals and experience the future of medical practice management. By signing up with Doctor's Digital, you gain access to a comprehensive suite of tools designed to streamline your practice, enhance patient care, and boost your efficiency.</p>
+                        <h2 className='hidden md:block text-xl md:text-2xl font-semibold mb-6'>Why choose us?</h2>
+                        <Accordian features={features} />
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
             <CommonFooter />
         </div>
     )
